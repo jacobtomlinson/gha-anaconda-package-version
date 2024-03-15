dcd23c8bad292c4eafa69cc6d4a6f336d6234d42
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2024-03-15 11:49:45 +0000

    Update main.go

--- main.go
+++ main.go
@@ -26,7 +26,10 @@ func getEnvDefault(key, default_value string) string {
 }
 
 func setOutput(key string, value string) {
-	path = os.LookupEnv("GITHUB_OUTPUT")
+	path, err := os.LookupEnv("GITHUB_OUTPUT")
+	if err != nil {
+	        log.Fatal(err)
+	}
 
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
